15ded4d6039f4b89e8cff8ed0b76384b14fae6fc
crackajacknmac <crackajacknmac@gmail.com>
2026-04-10 14:17:14 -0700

    O: Continuing Block logic

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,13 @@
+from enum import Enum
+
+class BlockType(Enum):
+        PARAGRAPH = "paragraph"
+        HEADING = "heading"
+        CODE = "code"
+        QUOTE = "quote"
+        UNORDERED_LIST = "unordered_list"
+        ORDERED_LIST = "ordered_list"
+
 def markdown_to_blocks(markdown):
     final_blocks = []
     raw_blocks = markdown.split("\n\n")
@@ -5,3 +15,40 @@ def markdown_to_blocks(markdown):
         block = block.strip()
         final_blocks.append(block)
     return list(filter(None, final_blocks))
+
+def block_to_block_type(block):
+    if block.startswith("```\n") and block.endswith("```"):
+        return BlockType.CODE
+
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.HEADING
+
+    if block.startswith(">"):
+        lines = block.split("\n")
+        for line in lines:
+            if line.startswith(">"):
+                continue
+            else:
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+
+    if block.startswith("- "):
+        lines = block.split("\n")
+        for line in lines:
+            if line.startswith("- "):
+                continue
+            else:
+                return BlockType.PARAGRAPH
+        return BlockType.UNORDERED_LIST
+
+    if block.startswith("1. "):
+        lines = block.split("\n")
+        number = 1
+        for line in lines:
+            if line.startswith(f"{number}. "):
+                number += 1
+                continue
+            else:
+                return BlockType.PARAGRAPH
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
