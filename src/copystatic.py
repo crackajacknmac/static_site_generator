dca552a46405a10b526fd59a8d3340805b854849
crackajacknmac <crackajacknmac@gmail.com>
2026-05-05 08:51:35 -0700

    V: Rerouted source; project complete

--- src/copystatic.py
+++ src/copystatic.py
@@ -4,18 +4,18 @@ from pathlib import Path
 from markdown_blocks import block_to_block_type, markdown_to_blocks, markdown_to_html_node
 from htmlnode import HTMLNode
 
-def generator(static, public):
-    if not os.path.exists(public):
-        os.mkdir(public)
-    static_list = os.listdir(static)
-    for name in static_list:
-        static_file_path = os.path.join(static, name)
-        public_file_path = os.path.join(public, name)
-        if os.path.isfile(static_file_path):
-            shutil.copy(static_file_path, public_file_path)
-        if os.path.isdir(static_file_path):
-            os.mkdir(public_file_path)
-            generator(static_file_path, public_file_path)
+def generator(source_dir, dest_dir):
+    if not os.path.exists(dest_dir):
+        os.mkdir(dest_dir)
+    source_list = os.listdir(source_dir)
+    for name in source_list:
+        source_file_path = os.path.join(source_dir, name)
+        dest_file_path = os.path.join(dest_dir, name)
+        if os.path.isfile(source_file_path):
+            shutil.copy(source_file_path, dest_file_path)
+        if os.path.isdir(source_file_path):
+            os.mkdir(dest_file_path)
+            generator(source_file_path, dest_file_path)
 
 def extract_title(markdown):
     lines = markdown.split("\n")
@@ -26,7 +26,7 @@ def extract_title(markdown):
             return title
     raise Exception("no heading found")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}.")
     directory = os.path.dirname(dest_path)
     os.makedirs(directory, exist_ok=True)
@@ -39,20 +39,22 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(source_markdown)
     new_page = template.replace('{{ Title }}', title)
     new_page = new_page.replace('{{ Content }}', content)
+    new_page = new_page.replace('href="/', f'href="{basepath}')
+    new_page = new_page.replace('src="/', f'src="{basepath}')
     with open(dest_path, "w") as f:
         f.write(new_page)
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     source_content = os.listdir(dir_path_content)
     for each in source_content:
         source = os.path.join(dir_path_content, each)
         if os.path.isfile(source):
             destination_path = os.path.join(dest_dir_path, each)
             destination_path = Path(destination_path).with_suffix(".html")
-            generate_page(source, template_path, destination_path)
+            generate_page(source, template_path, destination_path, basepath)
         else:
             recursed_destination = os.path.join(dest_dir_path, each)
-            generate_pages_recursive(source, template_path, recursed_destination)
+            generate_pages_recursive(source, template_path, recursed_destination, basepath)
 
 
--- src/main.py
+++ src/main.py
@@ -1,14 +1,17 @@
 import os
 import shutil
+import sys
 from pathlib import Path
 from copystatic import generator, extract_title, generate_page, generate_pages_recursive
 
 def main():
-
-    if os.path.exists("public"):
-        shutil.rmtree("public")
-    os.mkdir("public")
-    generator("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
+    basepath = "/"
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    if os.path.exists("docs"):
+        shutil.rmtree("docs")
+    os.mkdir("docs")
+    generator("static", "docs")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
 
 main()
